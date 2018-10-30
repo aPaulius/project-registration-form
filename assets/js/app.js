282cef842ee401dd3cf443c432f4bd0cba1ab368
pauliusaleksiunas <paulius.aleksiunas@gmail.com>
2018-10-30 18:24:34 +0200

    remove unused code from app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,9 +1,5 @@
 require('../css/app.css');
 
-// require('bootstrap');
-// require('inspinia');
-
-// TODO: add to page specific
 var measureHandler = require('./measureHandler');
 
 $(document).ready(function() {
